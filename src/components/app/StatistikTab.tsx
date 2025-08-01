72a8a785db4ae4f85b20ee229c44584bebe13011
Nick Simon <Nick.Simon@metalog.de>
2025-08-01 12:08:49 +0200

    Refactor StatistikTab to directly use versand in calculation

--- src/components/app/StatistikTab.tsx
+++ src/components/app/StatistikTab.tsx
@@ -69,9 +69,7 @@ export default function StatistikTab({ eintraege }: StatistikTabProps) {
       const actualRueckversand = eintrag.tatsaechliches_rueckgabedatum ? new Date(eintrag.tatsaechliches_rueckgabedatum) : (eintrag.rueckversand ? new Date(eintrag.rueckversand) : null);
 
       if (actualRueckversand) {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const versand = new Date(eintrag.versand);
-        const duration = (actualRueckversand.getTime() - versand.getTime()) / (1000 * 3600 * 24);
+        const duration = (actualRueckversand.getTime() - new Date(eintrag.versand).getTime()) / (1000 * 3600 * 24);
         if (durations[eintrag.tool]) {
           durations[eintrag.tool].push(duration);
         } else {
